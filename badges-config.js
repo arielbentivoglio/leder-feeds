2b8cd66bb52711a8ae93b42145ddd97735f062ce
arielbentivoglio <86375214+arielbentivoglio@users.noreply.github.com>
2026-05-20 12:08:32 -0300

    Badges config update 2026-05-20 12:08

--- badges-config.js
+++ badges-config.js
@@ -1,1 +1,1 @@
-window.BADGES_CONFIG = [{"ids_url": "https://arielbentivoglio.github.io/leder-feeds/ofertas-flash-ids.js", "var_name": "OFERTAS_FLASH_IDS", "text": "🔥OFERTA BOMBA", "bg_color": "#030303", "text_color": "#ffffff"}];
+window.BADGES_CONFIG = [];
