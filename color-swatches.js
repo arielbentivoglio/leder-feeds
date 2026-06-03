ea0549c02fae6c65302dfe3ae3552d5fa69839c5
arielbentivoglio <86375214+arielbentivoglio@users.noreply.github.com>
2026-06-03 15:15:12 -0300

    update color-swatches.js [SyncPropio]

--- color-swatches.js
+++ color-swatches.js
@@ -81,7 +81,8 @@
   "Punta Color": { t: "s", c: "#FEEEB8" },
   "Rosa y Blanco": { t: "d", a: "#FF69B4", b: "#ffffff" },
   "Suela": { t: "s", c: "#b87400" },
-  "Suela/Habano Claro": { t: "d", a: "#b87400", b: "#aa7444" }
+  "Suela/Habano Claro": { t: "d", a: "#b87400", b: "#aa7444" },
+  "Suela Crash": { t: "s", c: "#b87400" }
   };
 
   var SZ = '16px';
