07e6bc957c05b115ecc0d2e4297a1c61ddcad582
arielbentivoglio <86375214+arielbentivoglio@users.noreply.github.com>
2026-06-03 14:20:10 -0300

    update color-swatches.js [SyncPropio]

--- color-swatches.js
+++ color-swatches.js
@@ -60,7 +60,8 @@
   "Leder Rosa": { t: "s", c: "#FF69B4" },
   "Leder Tiza": { t: "s", c: "#FFFFF1" },
   "Leder Verde Claro": { t: "s", c: "#64c27b" },
-  "Beige": { t: "s", c: "#e8c39e" }
+  "Beige": { t: "s", c: "#e8c39e" },
+  "Manteca": { t: "s", c: "#FEEEB8" }
   };
 
   var SZ = '16px';
