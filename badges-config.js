3e2b60caf95afeaaf684367905f98bb6ed1af035
arielbentivoglio <86375214+arielbentivoglio@users.noreply.github.com>
2026-07-01 13:18:38 -0300

    Badges config update 2026-07-01 13:18

--- badges-config.js
+++ badges-config.js
@@ -1,1 +1,1 @@
-window.BADGES_CONFIG = [];
+window.BADGES_CONFIG = [{"ids_url": "https://arielbentivoglio.github.io/leder-feeds/winter-sale-ids.js", "var_name": "WINTER_SALE_IDS_2", "text": "❄️ WINTER SALE", "bg_color": "#000000", "text_color": "#ffffff", "border_color": "#000000", "pos_v": "bottom", "pos_h": "left"}];
