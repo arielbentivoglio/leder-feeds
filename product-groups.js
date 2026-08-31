7ae5059d1b1bb432c78388dca78ef2ecc14f3d75
arielbentivoglio <86375214+arielbentivoglio@users.noreply.github.com>
2026-08-31 14:51:24 -0300

    Actualiza product-groups.js (2026-08-31 14:51)

--- product-groups.js
+++ product-groups.js
@@ -1,7 +1,7 @@
 /**
  * product-groups.js - generado automaticamente por SyncPropio (Variantes Vinculadas)
  * No editar a mano: se pisa en la proxima publicacion desde el panel.
- * Generado: 2026-08-31T14:47:49.577877
+ * Generado: 2026-08-31T14:51:21.415970
  */
 (function () {
   "use strict";
